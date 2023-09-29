81db99fba8b459b1dc5e83d3e2ffb8a07377f0c4
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-09-29 19:28:37 -0300

    first commit

---
+++ src/componentes/Topo/index.js
@@ -0,0 +1,14 @@
+export default function Topo () {
+    return (
+        <header>
+            <div>
+                <img src="./assets/logo.png" alt="logo"/>
+                <nav>
+                    <a href="#produtos">PRODUTOS</a>
+                    <a href="#sobre">SOBRE</a>
+                    <a href="#contato">CONTATO</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
