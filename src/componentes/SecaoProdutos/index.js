511320be8a02ef03af05f6d2cd06292e1d4c10e8
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-09-30 11:47:15 -0300

    Criação da SecaoProdutos

--- src/componentes/SecaoProdutos/index.js
+++ src/componentes/SecaoProdutos/index.js
@@ -1,7 +1,42 @@
 export default function SecaoProdutos () {
     return (
         <section>
-            
+            <div>
+                <h2>Nossos produtos</h2>
+                <p>Trabalhamos com óculos de grau, óculos de sol, lentes transition nos modelos masculino, feminino e infantil.</p>
+                <p>Todos os nossos preços são acessíveis e contam com a melhor qualidade do mercado.</p>
+
+                <figure>
+                    <div>
+                        <h2>Óculos de grau</h2>
+                        <img src="./assets/oculos01.png"/>
+                        <p>R$ 500,00</p>
+                    </div>
+                    <div>
+                        <h2>Óculos transition</h2>
+                        <img src="./assets/oculos02.png"/>
+                        <p>R$ 750,00</p>
+                    </div>
+                    <div>
+                        <h2>Óculos de sol</h2>
+                        <img src="./assets/oculos03.png"/>
+                        <p>R$ 700,00</p>
+                    </div>
+                    <div>
+                        <h2>Óculos infantil</h2>
+                        <img src="./assets/oculos04.png"/>
+                        <p>R$ 500,00</p>
+                    </div>
+                </figure>
+
+                <p>Todos os nossos produtos incluem:</p>
+                <ul>
+                    <li>Garantia de 1 ano</li>
+                    <li>Manutenção preventiva</li>
+                    <li>Descontos especiais na compra da segunda unidade</li>
+                    <li>Flexibilidade de pagamento</li>
+                </ul>
+            </div>
         </section>
     );
 }
