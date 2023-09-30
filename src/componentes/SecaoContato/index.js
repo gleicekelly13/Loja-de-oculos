d0367123c809852f65713558f9b4383954e6250b
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-09-30 12:19:01 -0300

    Criação da SecaoContato

--- src/componentes/SecaoContato/index.js
+++ src/componentes/SecaoContato/index.js
@@ -1,7 +1,45 @@
 export default function SecaoContato () {
     return (
         <section>
-            
+            <div>
+                <h2>Fale conosco</h2>
+                <p>Não perca tempo, venha conhecer uma de nossas lojas ou entre em contato através de nossas redes sociais ou da central de atendimento.</p>
+
+                <div>
+                    <div>
+                        <h3>Contato</h3>
+                        <div>
+                            <img src="./assets/local.png"/>
+                            <p>Nova Iguaçu, RJ</p>
+                        </div>
+                        <div>
+                            <img src="./assets/telefone.png"/>
+                            <p>(21) 9999-9999</p>
+                        </div>
+                        <div>
+                            <img src="./assets/email.png"/>
+                            <p>contato@oticavida.com</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3>Nossas Redes Sociais</h3>
+                        <div>
+                            <img src="./assets/fb.png"/>
+                            <p>/OticaVida</p>
+                        </div>
+                        <div>
+                            <img src="./assets/ig.png"/>
+                            <p>@oticavidarj</p>
+                        </div>
+                        <div>
+                            <img src="./assets/tt.png"/>
+                            <p>@oticavidarj</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </section>
     );
 }
