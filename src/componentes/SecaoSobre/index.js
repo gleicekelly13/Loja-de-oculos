dd475c03e2823189b722f3d36e1302c06dffc19e
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-09-30 12:00:01 -0300

    Criação da SecaoSobre

--- src/componentes/SecaoSobre/index.js
+++ src/componentes/SecaoSobre/index.js
@@ -1,7 +1,23 @@
 export default function SecaoSobre () {
     return (
         <section>
-            
+            <div>
+                <h2>QUEM SOMOS NÓS?</h2>
+                <p>Fundada em 2001, em Nova Iguaçu - Rio de Janeiro, a Óticas vida iniciou suas atividades focada no atendimento ao público de renda mais baixa, sempre com o objetivo de proporcionar ao cliente bom atendimento, qualidade e preço baixo.</p>
+
+                <div>
+                    <img src="./assets/loja.png"/>
+                    <div>
+                        <h2>Nossas Filiais</h2>
+                        <p>Hoje temos mais de 20 filiais pelo Brasil e na América</p>
+                    </div>
+                    <div>
+                        <h2>Atendimento flexível</h2>
+                        <p>Nossa equipe possui treinamento para te atender</p>
+                    </div>
+                    <img src="./assets/atendimento.png"/>
+                </div>
+            </div>
         </section>
     );
 }
