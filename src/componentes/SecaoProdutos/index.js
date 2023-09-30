215baa48d64d7c8d881a977a096a5085e3cf06a9
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-09-30 11:16:16 -0300

    Criação das seções do conteúdo

---
+++ src/componentes/SecaoProdutos/index.js
@@ -0,0 +1,7 @@
+export default function SecaoProdutos () {
+    return (
+        <section>
+            
+        </section>
+    );
+}
---
+++ src/componentes/SecaoSobre/index.js
@@ -0,0 +1,7 @@
+export default function SecaoSobre () {
+    return (
+        <section>
+            
+        </section>
+    );
+}
